62f1f14e48cd5dd08838df1095a7c56106957b16
AverySims <avery.j.sims@gmail.com>
2023-09-11 13:49:41 -0500

    massive file update
    added Helper file
    added Employee files
    added program loop

--- EmployeeProgram/EmployeeProgram/Employee.cs
+++ EmployeeProgram/EmployeeProgram/Employee.cs
@@ -1,6 +1,69 @@
 ﻿namespace EmployeeProgram;
 
+// Base class: Employee
 public class Employee
 {
-    
+    public string Name { get; set; }
+    public string Title { get; set; }
+    public double Salary { get; set; }
+
+    public Employee(string name, string title, double salary)
+    {
+        Name = name;
+        Title = title;
+        Salary = salary;
+    }
+
+    public virtual double CalculateMonthlySalary()
+    {
+        return Salary / 12.0;
+    }
+}
+
+// Derived class: Manager
+public class Manager : Employee
+{
+    public int Bonus { get; set; }
+
+    public Manager(string name, string title, double salary, int bonus) : base(name, title, salary)
+    {
+        Bonus = bonus;
+    }
+
+    public override double CalculateMonthlySalary()
+    {
+        return (Salary + Bonus) / 12.0;
+    }
+}
+
+// Derived class: Engineer
+public class Engineer : Employee
+{
+    public int OvertimeHours { get; set; }
+
+    public Engineer(string name, string title, double salary, int overtimeHours) : base(name, title, salary)
+    {
+        OvertimeHours = overtimeHours;
+    }
+
+    public override double CalculateMonthlySalary()
+    {
+        return (Salary + (OvertimeHours * 20)) / 12.0;
+    }
+}
+
+// Derived class: Salesperson
+public class Salesperson : Employee
+{
+    public double Commission { get; set; }
+
+    public Salesperson(string name, string title, double salary, double commission) : base(name, title, salary)
+    {
+        Commission = commission;
+    }
+
+    public override double CalculateMonthlySalary()
+    {
+        return (Salary + (Commission * 0.1)) / 12.0;
+    }
 }
--- EmployeeProgram/EmployeeProgram/Program.cs
+++ EmployeeProgram/EmployeeProgram/Program.cs
@@ -4,7 +4,24 @@
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			// Create a list of employees
+			List<Employee> employees = new List<Employee>
+			{
+				new Manager("John", "Manager", 60000, 5000),
+				new Engineer("Alice", "Engineer", 50000, 10),
+				new Salesperson("Bob", "Salesperson", 45000, 2000)
+			};
+
+			// Calculate and display the total monthly salary for all employees
+			double totalMonthlySalary = 0;
+			foreach (var employee in employees)
+			{
+				double monthlySalary = employee.CalculateMonthlySalary();
+				Console.WriteLine($"{employee.Name} ({employee.Title}): Monthly Salary = ${monthlySalary:F2}");
+				totalMonthlySalary += monthlySalary;
+			}
+
+			Console.WriteLine($"Total Monthly Salary for All Employees: ${totalMonthlySalary:F2}");
 		}
 	}
 }
